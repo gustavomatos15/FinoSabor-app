1cd353ef34470ff39ece3121c3e9e3452e5aea19
gustavomatos15 <60790167+gustavomatos15@users.noreply.github.com>
2021-07-30 12:19:05 -0300

    atualizações TELA HOME

--- src/app/pedido/carrinho/carrinho.component.ts
+++ src/app/pedido/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItensPedido } from 'src/app/shared/models/itens-pedido';
 import { Pedido } from 'src/app/shared/models/pedido';
 import { CarrinhoUtil } from 'src/app/shared/utils/carrinho.util';
 import { environment } from 'src/environments/environment';
@@ -16,9 +17,37 @@ export class CarrinhoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.carrinho =  CarrinhoUtil.get();
+    this.carrinho = CarrinhoUtil.get();
   }
 
-  
+  remover(item) {
+
+    this.carrinho.itens.splice(item, 1);
+
+    CarrinhoUtil.update(this.carrinho);
+  }
+
+  addItem(produto: ItensPedido) {
+    let cart = CarrinhoUtil.get();
+    let position = cart.itens.findIndex(x => x.id_produto == produto.id_produto);
+    if (position != -1)
+      cart.itens[position].quantidade++;
+
+    CarrinhoUtil.update(cart);
+    this.carrinho = cart;
+  }
+
+
+  removeItem(produto: ItensPedido) {
+    if (produto.quantidade > 1) {
+      let cart = CarrinhoUtil.get();
+      let position = cart.itens.findIndex(x => x.id_produto == produto.id_produto);
+      if (position != -1)
+        cart.itens[position].quantidade--;
+
+      CarrinhoUtil.update(cart);
+      this.carrinho = cart;
+    }
+  }
 
 }
